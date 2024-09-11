1e162e4614c2e1dc953c6766e1eb4e3aad932e01
mdg680 <monoper@gmai.com>
2024-09-11 20:31:23 +0200

    Sample program to test debugging setup in VS Code.
    Externally provided code (not mine, i would never write functions,
    variable names and comments in Danish(!?). Just added for testing
    debugging in vs code with C#. Works just fine

---
+++ debug_test/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace DebugDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Debug demo");
+
+            // Variabel
+            int x = 10;
+            Console.WriteLine($"x = {x}");
+            x++;
+            Console.WriteLine($"x = {x}");
+            x = x * 2;
+            Console.WriteLine($"x = {x}");
+
+            Console.WriteLine("Tæller til 10 og retter farve");
+            for (int i = 0; i < 4; i++)
+            {
+                if (i < 2)
+                {
+                    Console.WriteLine("i er < 2");
+                }
+                else
+                {
+                    Console.WriteLine("i er >= 2");
+                }
+                Console.WriteLine(i);                
+            }
+            // Kalder metode
+            int res = LæggerToTalSammen(2, 2);
+            Console.WriteLine($"Resultat = {res}");
+
+            // kalder metode (der kalder metode)
+            MetodeA();
+
+            // Break ud i kode er også muligt
+            // System.Diagnostics.Debugger.Break();
+
+        }
+
+        static int LæggerToTalSammen(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void MetodeA()
+        {
+            Console.WriteLine("I metode A");
+            MetodeB();
+        }
+        static void MetodeB()
+        {
+            Console.WriteLine("I metode B");
+            MetodeC();
+        }
+        static void MetodeC()
+        {
+            Console.WriteLine("I metode C");
+        }
+
+    }
+}
